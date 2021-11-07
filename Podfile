41796382632e5de1cd209cdf17cd5d76a1cd4faa
yusuf <ysfozturk3@gmail.com>
2021-11-07 17:00:26 +0100

    added Firebase and created main structure

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Share_Firebase' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+# add pods for desired Firebase products
+# https://firebase.google.com/docs/ios/setup#available-pods
+pod 'Firebase/Auth'
+pod 'Firebase/Storage'
+  # Pods for Share_Firebase
+
+end
--- Share_Firebase/ViewController.swift
+++ Share_Firebase/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var passwordText: UITextField!
+    
+    @IBOutlet weak var emailText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signInClicked(_ sender: Any) {
+        performSegue(withIdentifier: "toFeedVC", sender: nil)
+    }
+    
+    @IBAction func signUpClicked(_ sender: Any) {
+    }
 }
 
